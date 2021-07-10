3a187fac1610e564937a660c1df34a5607ff8c0e
Yoel Cornamusaz <ycornamu@c1r10s4.42lausanne.ch>
2021-07-10 19:28:43 +0200

    Add last ex(s) of c01

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,26 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/09 14:53:34 by ycornamu          #+#    #+#             */
-/*   Updated: 2021/07/10 10:15:38 by ycornamu         ###   ########.fr       */
+/*   Created: 2021/07/10 10:15:01 by ycornamu          #+#    #+#             */
+/*   Updated: 2021/07/10 10:15:05 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_rev_int_tab(int *tab, int size)
+void	ft_sort_int_tab(int *tab, int size);
+
+void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
+	int	j;
 	int	t;
 
 	i = 0;
-	while (i < (size / 2))
+	while (i < size)
 	{
-		t = tab[i];
-		tab[i] = tab[size - 1 - i];
-		tab[size - 1 - i] = t;
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] > tab[j])
+			{
+				t = tab[i];
+				tab[i] = tab[j];
+				tab[j] = t;
+			}
+			j++;
+		}
 		i++;
 	}
 }
