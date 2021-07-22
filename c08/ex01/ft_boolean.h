1b80324e5da13962fdeac112c8538bdeedff577c
Yoel Cornamusaz <ycornamu@c1r10s4.42lausanne.ch>
2021-07-22 19:51:03 +0200

    Fix c08 ex01

--- c08/ex01/ft_boolean.h
+++ c08/ex01/ft_boolean.h
@@ -8,8 +8,8 @@ typedef char	t_bool;
 # define TRUE 1
 # define FALSE 0
 
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 
 # define SUCCESS 0
 
